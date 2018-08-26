423ed8d2176e527a5cdc4f013b977d7d566022b0
gonzalo <gonzalo_rojas@ucol.mx>
2018-08-26 15:01:42 -0500

    multiplos
    multiplos

---
+++ programa.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   int num1 = 2, num2 = 6;
+
+   if ( num2 % num1 == 0 )
+        cout<<num1<<" ES MULTIPLO DE "<<num2<<endl;
+    else
+        cout<<num1<<" NO ES MULTIPLO DE "<<num2<<endl;
+
+  return 0;
+}
+
